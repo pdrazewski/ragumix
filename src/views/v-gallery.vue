37e5ecaa8b9a240676ecba3f509299f3a4dbbaaa
pdrazewski <przemyslaw@MacBook-Air-Karolina.local>
2017-05-31 21:03:36 +0200

    Add lodash, add simple sorting with transition

--- src/views/v-gallery.vue
+++ src/views/v-gallery.vue
@@ -1,12 +1,16 @@
 <template>
 	<viewBase class="v-gallery">
 		<template slot>
-			<div v-for="(item, index) in items" style="float: left; padding: 40px;">
+			<button @click="filterByNameAsc">sortuje A-Z</button>
+			<button @click="filterByNameDesc">sortuje Z-A</button>
+			<transition-group name="flip-list" tag="ul">
+			<li v-bind:key="item" v-for="(item, index) in items" style="float: left; padding: 40px;border: 1px solid #eee">
 				<div :style="item.style">
 					{{item.name}}
 				</div>
 				<a href="#" class="m-team_remove" @click.prevent="removeTeam(item.key)">Remove</a>
-			</div>
+			</li>
+			</transition-group>
 		</template>
 	</viewBase>
 </template>
@@ -14,8 +18,9 @@
 <script>
 	import viewBase from './v-base.vue'
 	import firebaseConnect from '../firebase.js'
+	import _ from 'lodash'
 	export default {
-		name: 'view-code-add',
+		name: 'view-rects-gallery',
 		components: {
 			viewBase
 		},
@@ -32,6 +37,12 @@
 				firebaseConnect.helpers.removeKey('items', key)
 				firebaseConnect.helpers.fetchDB('items', this)
 			},
+			filterByNameAsc() {
+				this.items = _.sortBy(this.items, [function(o) { return o.name; }]);
+			},
+			filterByNameDesc() {
+				this.items = _.sortBy(this.items, [function(o) { return o.name; }]).reverse();
+			}
 		}
 	}
 </script>
