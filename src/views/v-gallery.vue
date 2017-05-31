64f35487578fe1cfa90384fd57a1fd914b176793
pdrazewski <przemyslaw@MacBook-Air-Karolina.local>
2017-05-31 22:25:17 +0200

    Add all sorting options

--- src/views/v-gallery.vue
+++ src/views/v-gallery.vue
@@ -4,10 +4,17 @@
 			<button @click="filterByNameAsc">sortuje A-Z</button>
 			<button @click="filterByNameDesc">sortuje Z-A</button>
 			<button @click="filterByWidth">sortuje width</button>
+			<button @click="filterByWidthDesc">sortuje width 2</button>
+			<button @click="filterByHeight">sortuje Height</button>
+			<button @click="filterByHeightDesc">sortuje Height 2</button>
+			<button @click="filterByRadius">sortuje radius</button>
+			<button @click="filterByRadiusDesc">sortuje radius 2</button>
+			<button @click="filterByDate">sortuje najstarsze </button>
+			<button @click="filterByDateDesc">sortuje najnowsze</button>
 			<transition-group name="flip-list" tag="ul">
 			<li v-bind:key="item" v-for="(item, index) in items" style="float: left; padding: 40px;border: 1px solid #eee">
 				<div :style="item.style">
-					{{item.name}}
+					{{item}}
 				</div>
 				<a href="#" class="m-team_remove" @click.prevent="removeTeam(item.key)">Remove</a>
 			</li>
@@ -39,13 +46,34 @@
 				firebaseConnect.helpers.fetchDB('items', this)
 			},
 			filterByNameAsc() {
-				this.items = _.sortBy(this.items, [function(o) { return o.name; }]);
+				this.items = _.sortBy(this.items, [function(o) { return o.name.toLowerCase(); }]);
 			},
 			filterByNameDesc() {
-				this.items = _.sortBy(this.items, [function(o) { return o.name; }]).reverse();
+				this.items = _.sortBy(this.items, [function(o) { return o.name.toLowerCase(); }]).reverse();
 			},
 			filterByWidth() {
-				this.items = _.sortBy(this.items, [function(o) { return o.style.width; }]);
+				this.items = _.sortBy(this.items, [function(o) { return parseInt(o.style.width); }]);
+			},
+			filterByWidthDesc() {
+				this.items = _.sortBy(this.items, [function(o) { return parseInt(o.style.width); }]).reverse();
+			},
+			filterByHeight() {
+				this.items = _.sortBy(this.items, [function(o) { return parseInt(o.style.height); }]);
+			},
+			filterByHeightDesc() {
+				this.items = _.sortBy(this.items, [function(o) { return parseInt(o.style.height); }]).reverse();
+			},
+			filterByRadius() {
+				this.items = _.sortBy(this.items, [function(o) { return parseInt(o.style["border-radius"]); }]);
+			},
+			filterByRadiusDesc() {
+				this.items = _.sortBy(this.items, [function(o) { return parseInt(o.style["border-radius"]); }]).reverse();
+			},
+			filterByDate() {
+				this.items = _.sortBy(this.items, [function(o) { return o.createdAt; }]);
+			},
+			filterByDateDesc() {
+				this.items = _.sortBy(this.items, [function(o) { return o.createdAt; }]).reverse();
 			}
 		}
 	}
